5ef1d5dee7a75ebee0498ae737ac92c001b94a56
fabio eduardo <circuncisaofabio@icloud.com>
2023-03-15 19:38:07 -0300

    foi adicionado o getTicket que pega o ticket fazendo uma request e colcando na variavbl data e a function all que faz a mesma coisa porem recebe todos os ticket, function getdata era pra modificar mas nao esta completo

--- app/assets/src/App.js
+++ app/assets/src/App.js
@@ -13,5 +13,46 @@ const textarea = document.createElement("textarea")
 textarea.setAttribute('maxlength', 5000);
 textarea.setAttribute('cols', 80);
 textarea.setAttribute('rows', 40);
+
+const editButton = document.createElement("button");
+editButton.name = "Look"
+editButton.innerHTML = "Click";
+editButton.style.position = "absolute";
+editButton.style.display = "none";
+editButton.onclick((e) =>{
+  getData()
+})
+
+function getTicket(){
+  try {
+   const data= client.request('/api/v2/tickets.json').then(function(tickets){
+    console.log(data)
+    return data
+    })
+  } catch (error) {
+    console.log(error)
+  }
+
+  return data
+}
+
+functio getAll(){
+ const data =  client.get('ticket.subject').then(created_at =>{
+  console.log("TICKETDATA",  "ID=" + ticket.ticket.id, "Subject=" +ticket.ticket.subject);
+  this.ticketData= ticket;
+  return data
+  })
+  return data
+}
+
+
+function getData(){
+
+  
+}
+textarea(data)
+editButton()
+div.appendChild(textarea)
+div.appendChild(editButton)
 import Main from "./Main.js";
 Main();
