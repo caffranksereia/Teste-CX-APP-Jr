971731fada116716d91577523feaa7ccaa356d94
fabio eduardo <circuncisaofabio@icloud.com>
2023-03-15 19:24:35 -0300

    foram adicionados o campo texto e a config top Bar Client Promise

--- app/assets/src/App.js
+++ app/assets/src/App.js
@@ -9,5 +9,9 @@ topBarClientPromise.then(function(topBarClient) {
   topBarClient.invoke('popover');
 });
 
+const textarea = document.createElement("textarea")
+textarea.setAttribute('maxlength', 5000);
+textarea.setAttribute('cols', 80);
+textarea.setAttribute('rows', 40);
 import Main from "./Main.js";
 Main();
